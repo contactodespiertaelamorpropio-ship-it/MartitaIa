32bf47b0f94280520d16ea9aaf8df379969290f8
contactodespiertaelamorpropio-ship-it <contacto.despiertaelamorpropio@gmail.com>
2026-02-03 21:11:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// Mostrar ventana automáticamente
-window.onload = function() {
-  document.getElementById("iaWindow").style.display = "flex";
-  
-  let nombre = localStorage.getItem("nombreUsuario");
-  if (!nombre) {
-    nombre = prompt("¿Cuál es tu nombre?");
-    if (nombre) {
-      localStorage.setItem("nombreUsuario", nombre);
-      document.getElementById("saludo").innerText = "💗 Hola " + nombre + ", soy Martita";
-      document.getElementById("subSaludo").innerText = "Tu compañera de Amor Propio";
-    }
-  } else {
-    document.getElementById("saludo").innerText = "💗 Hola " + nombre + ", soy Martita";
-    document.getElementById("subSaludo").innerText = "Tu compañera de Amor Propio";
-  }
-}
-
 function toggleSection(id) {
   const section = document.getElementById(id);
   section.classList.toggle("hidden");
@@ -33,3 +15,4 @@ function volverMenu() {
   document.getElementById("textBox").classList.add("hidden");
   document.getElementById("volverBtn").classList.add("hidden");
 }
+
